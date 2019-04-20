f2e31b559593ff2c9f8ed6ce3f481744b0b87f6e
0Oo-oO0 <46864199+0Oo-oO0@users.noreply.github.com>
2019-04-20 23:36:09 +0800

    树树

---
+++ 手游组-刘志鸿-训练营-二叉树/手游组-刘志鸿-训练营-二叉树/main.cpp
@@ -0,0 +1,13 @@
+#include"main.h"
+
+int main()
+{
+	BinaryTree<int> tree1;
+	int a;
+	a = tree1.Value();
+	std::cout << a << std::endl;
+
+
+	system("PAUSE");
+	return 0;
+}
---
+++ 手游组-刘志鸿-训练营-二叉树/手游组-刘志鸿-训练营-二叉树/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include"BinaryTree.h"
+#include<iostream>
+#include<stdlib.h>
+
+#endif // !_MAIN_H_
+
